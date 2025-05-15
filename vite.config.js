54796d0b1fe43d0d849207d822b93fe47bc08585
Bibhut <erbthakuri@gmail.com>
2025-05-15 15:48:31 +0545

    add /

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: '/bibhut-portfolio', 
+  base: '/bibhut-portfolio/', 
   resolve:{
     alias:{
       "@": path.resolve(__dirname, "./src"),
